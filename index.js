ebc4939e7ce976d9563865a48e18275a0fd12fdd
mashudalhasan <mashudalhasan554@gmail.com>
2023-05-20 19:17:25 +0600

    update functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c5zhdwf.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +24,68 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("toyLand").collection("products");
+
+    app.get("/products", async (req, res) => {
+      await productCollection.updateMany({}, [
+        {
+          $set: {
+            price: { $toDouble: "$price" },
+          },
+        },
+      ]);
+      const query = {};
+      const cursor = productCollection.find(query).sort({ price: 1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // sorting with price
+    app.get("/toys", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await productCollection
+        .find(query)
+        .sort({ price: 1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedToys = req.body;
+      const toys = {
+        $set: {
+          price: updatedToys.price,
+          quantity: updatedToys.quantity,
+          description: updatedToys.description,
+        },
+      };
+
+      const result = await productCollection.updateOne(filter, toys, options);
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newToy = req.body;
+      console.log(newToy);
+      const result = await productCollection.insertOne(newToy);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +93,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
